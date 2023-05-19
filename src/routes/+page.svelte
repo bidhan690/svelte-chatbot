1c4b005069589b8aebcd12db41133e60ce8ae1d6
bidhan <bidhan@kali.domain.name>
2023-05-19 17:11:29 +0545

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1
+  class="text-5xl text-indigo-500 flex items-center justify-center h-screen font-[600] text-transparent bg-clip-text bg-gradient-to-br from-[#a22fce] to-[#ff7000]"
+>
+  Start building apps with Svelte + Tailwind CSS
+</h1>
